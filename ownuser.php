0e13cf20354cd86038eb324f45b38f957b4fd46d
Imran Niaz <80633793+imran-niaz@users.noreply.github.com>
2022-12-16 18:17:57 -0500

    Use Die Function in PHP

--- ownuser.php
+++ ownuser.php
@@ -3,14 +3,37 @@
 // Get the visitor's IP address
 $ip = $_SERVER['REMOTE_ADDR'];
 
-// Check if the IP address is on the list of blocked IPs
-$blocked_ips = array('123.456.789.0', '987.654.321.0');
-if (in_array($ip, $blocked_ips)) {
-  // If the IP is on the list, block the user's access
+// Check if the visitor is doing suspicious activity
+if (is_suspicious_activity($ip)) {
+  // If the visitor is doing suspicious activity, log the IP address
+  log_ip_address($ip);
+
+  // Block the visitor's access
   die('Your IP address has been blocked due to suspicious activity.');
 }
 
 // Continue with the rest of the script
 // ...
+
+// Function to check if the visitor is doing suspicious activity
+function is_suspicious_activity($ip) {
+  // Check for suspicious activity using various methods
+  // For example, you could check for an unusually high number of requests
+  // from the same IP, or for certain patterns in the request data.
+  // Return true if suspicious activity is detected, false otherwise.
+  return true;
+}
+
+// Function to log the IP address of a visitor doing suspicious activity
+function log_ip_address($ip) {
+  // Open the log file in append mode
+  $log_file = fopen('suspicious_activity.log', 'a');
+
+  // Write the IP address to the log file
+  fwrite($log_file, $ip . "\n");
+
+  // Close the log file
+  fclose($log_file);
+}
 
 ?>
