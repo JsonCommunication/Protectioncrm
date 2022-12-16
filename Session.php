93a7e76b4b6b4e2b5692289fc045798d0731fd19
Imran Niaz <80633793+imran-niaz@users.noreply.github.com>
2022-12-16 18:10:03 -0500

    Create Session.php

---
+++ Session.php
@@ -0,0 +1,9 @@
+<section>
+  <?php
+  // PHP code goes here
+  $section_title = "Welcome to our website";
+  $section_content = "Thank you for visiting our website. We hope you find the information you are looking for.";
+  ?>
+  <h2><?php echo $section_title; ?></h2>
+  <p><?php echo $section_content; ?></p>
+</section>
