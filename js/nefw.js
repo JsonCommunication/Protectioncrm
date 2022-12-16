ed8a5593abffeddf7b0371097e39b29342218923
Imran Niaz <80633793+imran-niaz@users.noreply.github.com>
2022-12-16 18:25:52 -0500

    Create nefw.js

---
+++ js/nefw.js
@@ -0,0 +1,38 @@
+<script>
+  // Get the visitor's IP address
+  const ip = '<?php echo $_SERVER['REMOTE_ADDR']; ?>';
+
+  // Check if the visitor is doing suspicious activity
+  if (isSuspiciousActivity(ip)) {
+    // If the visitor is doing suspicious activity, log the IP address and block the visitor's access
+    logIpAddress(ip);
+    blockAccess();
+  }
+
+  // Function to check if the visitor is doing suspicious activity
+  function isSuspiciousActivity(ip) {
+    // Check for suspicious activity using various methods
+    // For example, you could check for an unusually high number of requests
+    // from the same IP, or for certain patterns in the request data.
+    // Return true if suspicious activity is detected, false otherwise.
+    return true;
+  }
+
+  // Function to log the IP address of a visitor doing suspicious activity
+  function logIpAddress(ip) {
+    // Send an AJAX request to the server to log the IP address
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/log_ip_address.php');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(`ip=${encodeURIComponent(ip)}`);
+  }
+
+  // Function to block the visitor's access
+  function blockAccess() {
+    // Display an error message
+    alert('Your IP address has been blocked due to suspicious activity.');
+
+    // Redirect the visitor to the homepage
+    window.location.href = '/';
+  }
+</script>
