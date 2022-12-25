2452c9718c8c01eb85918e68683762090100c5ec
Imran Niaz <80633793+imran-niaz@users.noreply.github.com>
2022-12-25 05:37:06 -0500

    Update Session.php

--- Session.php
+++ Session.php
@@ -7,3 +7,55 @@
   <h2><?php echo $section_title; ?></h2>
   <p><?php echo $section_content; ?></p>
 </section>
+<?php
+
+// Connect to the database
+$db = mysqli_connect('localhost', 'user', 'password', 'database');
+
+// Check if the request method is POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // Validate the input data
+  if (!isset($_POST['question']) || !isset($_POST['customer_id'])) {
+    // Return an error if the data is missing or invalid
+    echo json_encode(array('success' => false, 'message' => 'Error: Invalid input data'));
+    exit;
+  }
+  $question = mysqli_real_escape_string($db, $_POST['question']);
+  $customer_id = mysqli_real_escape_string($db, $_POST['customer_id']);
+
+  // Check if the question has an answer in the database
+  $result = mysqli_query($db, "SELECT * FROM answers WHERE question='$question'");
+  if (mysqli_num_rows($result) > 0) {
+    // The question has an answer, so retrieve it from the database
+    $row = mysqli_fetch_assoc($result);
+    $answer = $row['answer'];
+
+    // Send the answer back to the customer
+    echo json_encode(array('success' => true, 'answer' => $answer));
+  } else {
+    // The question does not have an answer, so save it as feedback in the database
+    $result = mysqli_query($db, "INSERT INTO feedback (customer_id, question) VALUES ('$customer_id', '$question')");
+    if ($result) {
+      // Feedback was saved successfully
+      echo json_encode(array('success' => true, 'message' => 'Thank you for your feedback! We will review it and try to provide an answer as soon as possible.'));
+    } else {
+      // There was an error saving the feedback
+      echo json_encode(array('success' => false, 'message' => 'Error: '.mysqli_error($db)));
+    }
+  }
+} else {
+  // Return an error if the request method is not POST
+  echo json_encode(array('success' => false, 'message' => 'Error: Invalid request method'));
+}
+
+// Log the API request
+$log_file = fopen('api_log.txt', 'a');
+$request_data = array(
+  'method' => $_SERVER['REQUEST_METHOD'],
+  'question' => $question,
+  'customer_id' => $customer_id
+);
+fwrite($log_file, date('Y-m-d H:i:s').' '.json_encode($request_data)."\n");
+fclose($log_file);
+
+?>
